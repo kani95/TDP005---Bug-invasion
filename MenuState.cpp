572755c60c606933cf4f39a1ae1ab80fe7381a32
kasni325 <kasni325@student.liu.se>
2020-12-19 15:13:50 +0100

    No more warnings

--- MenuState.cpp
+++ MenuState.cpp
@@ -99,6 +99,8 @@ void MenuState::poll_events(sf::Event & event)
                     default:
                         break;
                 }
+            default:
+                break;
         }
     }
 }
